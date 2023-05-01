08d1f95c8dc660b8dd8f702e9df17f45e4990267
Shivam Singh <125348423+shivamknoldus1869@users.noreply.github.com>
2023-05-01 17:53:41 +0530

    add all file of Cypress
    Cypress to Jenkins Assignment

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,19 @@
+describe('Search Smartwatches on Flipkart', () => {
+    it('Searches and Displays Results', () => {
+      // Visit the Flipkart website
+      cy.visit('https://www.flipkart.com')
+  
+      // Enter the search query
+      cy.get('input[name="q"]')
+        .type('smartwatches')
+        .should('have.value', 'smartwatches')
+  
+      // Click the search button
+      cy.get('button[type="submit"]')
+        .click()
+  
+      // Assert that search results are displayed
+      cy.get('div._1YokD2._3Mn1Gg')
+        .should('be.visible')
+    })
+  })
